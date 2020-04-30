a4752d22e6c37aa9ccd718e91fa7faab126b132a
Pruthula <pruthula@gmail.com>
2020-04-30 15:16:36 +0530

    Completed new challlenges

--- src/com/hackerrank/solved/ReverseArray.java
+++ src/com/hackerrank/solved/ReverseArray.java
@@ -1,13 +1,10 @@
 package com.hackerrank.solved;
 
-import java.io.BufferedWriter;
-import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Scanner;
 
-public class Solution {
+public class ReverseArray {
 
-    // Complete the reverseArray function below.
     static int[] reverseArray(int[] a) {
 
 		if (a != null && a.length > 0) {
@@ -25,8 +22,7 @@ public class Solution {
     private static final Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) throws IOException {
-        //BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
-
+    	
         int arrCount = scanner.nextInt();
         scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
 
@@ -43,19 +39,15 @@ public class Solution {
         int[] res = reverseArray(arr);
 
         for (int i = 0; i < res.length; i++) {
-           // bufferedWriter.write(String.valueOf(res[i]));
         	System.out.print(res[i]);
 
             if (i != res.length - 1) {
-                //bufferedWriter.write(" ");
             	System.out.print(" ");
             }
         }
-/*
-        bufferedWriter.newLine();
-
-        bufferedWriter.close();*/
 
         scanner.close();
     }
+
+
 }
